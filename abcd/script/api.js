5a55a5502119db0af8c4dbcc9cd69eda24c91957
raviop8303 <rk8454111@gmail.com>
2026-02-13 14:55:43 +0530

    Change API URL from port 5000 to 3001

--- abcd/script/api.js
+++ abcd/script/api.js
@@ -1,5 +1,5 @@
 // API Configuration
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'http://localhost:3001/api';
 
 // Get token from localStorage
 const getToken = () => localStorage.getItem('token');
